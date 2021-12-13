76f0c03845f9f9a541cd83c44c6a1c923098c052
Ryan <wooryans@gmail.com>
2021-12-13 16:36:29 -0500

    Add varied_attn_heads script

---
+++ candbproj/gpt2_varied_attn_heads.py
@@ -0,0 +1,66 @@
+
+import logging
+import pickle
+from pathlib import Path
+
+import warnings
+from transformers import GPT2TokenizerFast, GPT2Model, GPT2Config
+import scipy
+
+
+from candbproj.score import score, normalize_scores
+from candbproj.feature_extractors import PassageTokenizer
+from candbproj.result import PereiraResult, Args
+
+logging.basicConfig()
+log = logging.getLogger()
+
+
+def main():
+
+    gpt2_trained_result = Path(__file__).parent.resolve() / "../results/gpt2_varied_attn_heads_result.pkl"
+    if gpt2_trained_result.exists():
+        with open(gpt2_trained_result, "rb") as f:
+            results = pickle.load(f)
+    else:
+        results = []
+        for heads in [1, 2, 4, 8, 12, 16, 32]:
+            model_config = GPT2Config.from_pretrained("gpt2")
+            model_config.n_head = heads
+            model_config.output_hidden_states = True
+
+            model = GPT2Model(model_config)
+            tokenizer_args = Args(
+                args=("gpt2",)
+            )
+            tokenizer = GPT2TokenizerFast.from_pretrained(*tokenizer_args.args)
+            feature_extractor = PassageTokenizer(tokenizer)
+            model = model.eval()
+
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", scipy.stats.PearsonRConstantInputWarning)  # Ignore the very many PearsonRCoefficient warnings
+                scores = score(model, feature_extractor)
+
+            result = PereiraResult(
+                scores=scores,
+                model_config=model_config,
+                tokenizer_args=tokenizer_args
+            )
+            results.append(result)
+
+        try:
+            with open(gpt2_trained_result, "wb") as f:
+                pickle.dump(results, f)
+        except OSError:
+            log.warning("Warning: could not write result to file", exc_info=True)
+
+    for result in results:
+        scores = result.scores
+        normalized_scores = normalize_scores(scores)
+
+
+        print(f"Attention Heads: {result.model_config.n_head}", scores, normalized_scores)
+
+
+if __name__ == "__main__":
+    main()
